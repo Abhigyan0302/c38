29b4de4748d989f2c099d385c0817f6b6de14bf3
Abhigyan0302 <58260534+Abhigyan0302@users.noreply.github.com>
2020-02-25 18:45:11 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,34 @@
+class Form{
+    constructor(){
+        this.input=createInput("name");
+        this.button=createButton('play');
+        this.greeting=createElement('h1');
+
+    }
+    hide(){
+        this.input.hide();
+        this.greeting.hide();
+        this.button.hide();
+    }
+    display(){
+        var title=createElement('h1');
+        title.html("car racing game");
+       title.position(displayWidth/2-50,0);
+       
+        this.input.position(displayWidth/2-40,displayHeight/2-70);
+        this.button.position(displayWidth/2+20,displayHeight/2);
+        this.button.mousePressed(()=>{
+            this.input.hide();
+           this.button.hide();
+            player.name=this.input.value();
+            playerCount+=1;
+            player.index=playerCount;
+            player.update();
+            player.updateCount(playerCount);
+             
+            this.greeting.html("Welcome "+ player.name);
+            this.greeting.position(displayWidth/2-60,displayHeight/3);
+
+        })
+    }
+}
